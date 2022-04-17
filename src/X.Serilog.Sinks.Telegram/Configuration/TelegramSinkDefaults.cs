69daea7bc88cf3e31d3454b038f3d23c001fa31d
Vladyslav Bardin <vbardin810@gmail.com>
2022-04-17 23:35:53 +0300

    Move extensions methods to a separated lib
    This is a breaking change without any back capability

--- src/X.Serilog.Sinks.Telegram/Configuration/TelegramSinkDefaults.cs
+++ src/X.Serilog.Sinks.Telegram/Configuration/TelegramSinkDefaults.cs
@@ -1,6 +1,6 @@
 ﻿namespace X.Serilog.Sinks.Telegram.Configuration;
 
-internal static class TelegramSinkDefaults
+public static class TelegramSinkDefaults
 {
     public const int BatchPostingLimit = 20;
     public static readonly TimeSpan BatchPostingPeriod = new(0, 0, 20);
